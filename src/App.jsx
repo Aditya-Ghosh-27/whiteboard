d0b4f2c8bd95404281b1b456f0b55b5f583414d1
Aditya Ghosh <adi27.ghosh@gmail.com>
2025-03-11 00:18:04 +0530

    fixed the folder structure - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import { useEffect, useRef } from "react";
-import rough from "roughjs";
+import Board from "./components/Board";
 
 function App(){
-  const canvasRef = useRef();
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const context = canvas.getContext("2d");
-
-    let roughCanvas = rough.canvas(canvas);
-    let generator = roughCanvas.generator;
-    let rect1 = generator.rectangle(10, 10, 100, 100);
-    let rect2 = generator.rectangle(10, 120, 100, 100, { fill: "blue" });
-    roughCanvas.draw(rect1);
-    roughCanvas.draw(rect2);
-
-  }, []);
   return(
-    <div className="App">
-      <canvas ref={canvasRef}/> 
-    </div>
+    <Board />
   );
 }
 
